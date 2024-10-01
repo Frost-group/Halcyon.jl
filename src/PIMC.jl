16adf7eadcb755deb9bb1c861a839834fa37a466
Jarvist Moore Frost <jarvist@gmail.com>
2024-10-01 11:26:59 +0100

    Some comments, as I remind myself of the code.

--- src/PIMC.jl
+++ src/PIMC.jl
@@ -1,8 +1,9 @@
 # PIMC.jl
-# Initially location for all the path integral bits and bobs
+# Initial location for all the path integral bits and bobs
 
-# Very generic path
+# Generic as possible path
 
+# Assumes SliceA, SliceB
 function KineticAction(sA,sB)
     KE=(sA.r .- sB.r).^2
     KE=KE/4*Lambda*tau
@@ -21,26 +22,26 @@ function localMove!(system,path; moves=1, verbose=false, stepsize=0.1)
     REJECT=0
 
     for m in 1:moves # this loop brought within the function
-        # I don't understand why, but otherwise you get a lot of allocations (12!) from
+        # I don't understand why, but otherwise you get a lot of allocations (12?!) from
         # dereferencing system.potential for every run of the function. 
 
-    particle=rand(1:system.nparticles)
-    bead=rand(1:system.nbeads)
-    Δ=stepsize*randn(system.spatialdimensions)
+        # pick random particle, random bead, attempted 'simple' move
+        particle=rand(1:system.nparticles)
+        bead=rand(1:system.nbeads)
+        Δ=stepsize*randn(system.spatialdimensions)
 
-    @inbounds begin
-    pold=@view path.r[particle,bead,:]
-    pnew=pold+Δ
-
-    
-    Sold = Harmonic(pold) +
+        @inbounds begin
+        pold=@view path.r[particle,bead,:]
+        pnew=pold+Δ
+        
+        Sold = Harmonic(pold) +
         sum(abs2, (pold .- @view path.r[path.prev[particle, bead], mod1(bead - 1, system.nbeads), :])) +
-        sum(abs2, (pold .- @view path.r[path.next[particle, bead], mod1(bead + 1, system.nbeads), :]))
+            sum(abs2, (pold .- @view path.r[path.next[particle, bead], mod1(bead + 1, system.nbeads), :]))
 
-    Snew = Harmonic(pnew) +
-        sum(abs2, (pnew .- @view path.r[path.prev[particle, bead], mod1(bead - 1, system.nbeads), :])) +
-        sum(abs2, (pnew .- @view path.r[path.next[particle, bead], mod1(bead + 1, system.nbeads), :]))
-    end
+        Snew = Harmonic(pnew) +
+            sum(abs2, (pnew .- @view path.r[path.prev[particle, bead], mod1(bead - 1, system.nbeads), :])) +
+            sum(abs2, (pnew .- @view path.r[path.next[particle, bead], mod1(bead + 1, system.nbeads), :]))
+        end
 
     ΔS=Snew-Sold
     
@@ -48,6 +49,7 @@ function localMove!(system,path; moves=1, verbose=false, stepsize=0.1)
         println("localMove! particle=$particle bead=$bead Δ=$Δ pold=$pold pnew=$pnew Sold=$Sold Snew=$Snew ΔS=$ΔS")
     end
 
+    # Metropolis critereon
     if ΔS ≤ 0 || rand() < exp(-system.β * ΔS)
         if verbose println("Accept!") end
         ACCEPT+=1
