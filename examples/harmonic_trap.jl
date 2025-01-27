39840b1cf2964a48a9c6ed67fd4b12d35d64ef6c
Jarvist Moore Frost <jarvist@gmail.com>
2025-01-27 16:36:36 +0000

    Add finite-T QHO.

--- examples/harmonic_trap.jl
+++ examples/harmonic_trap.jl
@@ -5,20 +5,21 @@ using Gnuplot
 using StatsBase
 
 # Simulation parameters
-const β = 10.0        # Inverse temperature
-const nbeads = 100    # Number of beads (M in Thijsesen - is this standard?)
-const nparticles = 1  # Number of particles
+const β = 1.0        # Inverse temperature
+const nbeads = 30    # Number of beads (M in Thijsesen - is this standard?)
+const nparticles = 100  # Number of particles
 const ω = 1.0         # Harmonic trap frequency
 const ℏ = 1.0         # Planck constant
 const m = 1.0         # Particle mass
-const EQUILIBRATION_STEPS = 2_000    # First 2000 steps discarded
+const EQUILIBRATION_STEPS = 2_000    # ... Thijssen, First 2000 steps discarded
 const MEASUREMENT_STEPS = 100        # Steps between measurements
-const MEASUREMENTS = 280             # To get 28000 post-equilibration steps
+const MEASUREMENTS = 280 #             # ... Thijssen, 28000 post-equilibration steps
 
-# Analytical solutions for 1D quantum harmonic oscillator
-qho_ground_state_energy(ω::Float64)=0.5 * ℏ * ω
-# Ground state probability density
-qho_ground_state_density(x::Float64, ω::Float64)=sqrt(ω / (π * ℏ)) * exp(-ω * x^2 / ℏ)
+# Analytical solutions for quantum harmonic oscillator
+qho_ground_state_energy(ω::Float64) = 0.5 * ℏ * ω
+qho_ground_state_density(x::Float64, ω::Float64) = sqrt(ω / (π * ℏ)) * exp(-ω * x^2 / ℏ)
+qho_finite_temp_energy(ω::Float64, β::Float64) = (ℏ * ω / 2) * coth(β * ℏ * ω / 2)
+qho_finite_temp_density(x::Float64, ω::Float64, β::Float64) = sqrt(m * ω / (2π * ℏ * sinh(β * ℏ * ω))) * exp(-(m * ω * x^2 / (2ℏ)) * tanh(β * ℏ * ω / 2))
 
 # Initialize system with correct parameters
 println("Initializing quantum particle in harmonic trap...")
@@ -35,7 +36,7 @@ path = Halcyon.Path(system)
 
 # Equilibration
 println("Equilibrating for $EQUILIBRATION_STEPS steps...")
-@time Halcyon.localMove!(system, path, moves=EQUILIBRATION_STEPS)
+@time Halcyon.localMove!(system, path, moves=EQUILIBRATION_STEPS, verbose=false)
 
 # Data collection
 println("Collecting measurements for $MEASUREMENT_STEPS steps...")
@@ -80,12 +81,19 @@ density = normalize(histogram.weights, 1) ./ bin_width
 # Plot!
 x_points = collect(range(-3, 3, length=100))
 sampled_qho_ground_state_density = qho_ground_state_density.(x_points, ω)
+sampled_qho_finite_temp_density = qho_finite_temp_density.(x_points, ω, β)
 centers = (edges[1:end-1] .+ edges[2:end]) ./ 2
 
 @gp "set title 'Position Distribution'"
 @gp "set xlabel 'Position'"
 @gp "set ylabel 'Probability Density'"
 @gp :- centers density "w histeps title 'PIMC'"
-@gp :- x_points sampled_qho_ground_state_density "w l title 'Ground state QHO'"
+@gp :- x_points sampled_qho_ground_state_density "w l title 'Ground state'"
+@gp :- x_points sampled_qho_finite_temp_density "w l title 'Finite temp'"
+
+potential(x) = 0.5 * m * ω^2 * x^2
+potential_values = potential.(x_points)
+
+@gp :- x_points potential_values "w l title 'potential'"
 
 println("\nPlots saved as 'harmonic_trap_results.png'") 
